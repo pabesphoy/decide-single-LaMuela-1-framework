b3997da5d12de414e9b48bb87dbb737d84c43953
pabesphoy <pabesphoy@alum.us.es>
2022-12-18 23:23:11 +0100

    fix:deployment
    urls.py actualizado

--- decide/decide_statistics/urls.py
+++ decide/decide_statistics/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('test_voting/', views.create_voting, name='test_create')
+    path('', views.index, name='index')
 ]
